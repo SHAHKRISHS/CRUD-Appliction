13053d865e9dde08f40c12cbaa3a87920959ff58
KRISH SHAH <79298491+SHAHKRISHS@users.noreply.github.com>
2022-09-26 10:44:58 +0530

    First Commit

---
+++ TodoApplication/TodoApplication/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApplication.Controllers
+{
+    public class TodoController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Models.TodoApp td)
+        {
+            ViewBag.id = td.Id;
+            ViewBag.Name = td.Name;
+            ViewBag.Number = td.Number;
+
+            return View("Index");
+        }
+
+
+
+    }
+}
---
+++ TodoApplication/TodoApplication/Models/TodoApp.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApplication.Models
+{
+    public class TodoApp
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? EmailId { get; set; }
+
+        [Required]
+        public string? Number { get; set; }
+    }
+}
